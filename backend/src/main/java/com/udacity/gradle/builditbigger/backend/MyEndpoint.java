e9367f2eab001698071b535fa4aadab2b1925432
Zukka <info@zukkadev.net>
2018-08-08 07:22:25 +0200

    Add free Flavor

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -19,14 +19,6 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
-        return response;
-    }
     @ApiMethod(name = "tellJoke")
     public MyBean tellJoke(){
         MyBean response = new MyBean();
